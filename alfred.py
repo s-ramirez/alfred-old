a68effdc9a84a4bb1d13f14e27e2bf72b2e204e4
Sebastian Ramirez <sramirez10r@gmail.com>
2017-01-05 15:14:17 -0600

    Fixing entity recognition for weather forecast

--- alfred.py
+++ alfred.py
@@ -7,5 +7,5 @@ data = TrainingData('skills')
 trainer.train(data)
 # print(trainer.parse("hola"));
 # print(trainer.parse("goodbye"));
-print(trainer.parse("Should I wear a sweater?"))
+print(trainer.parse("Is it raining should I wear a sweater?"))
 print(trainer.parse("I'm looking for a place in the north of town"));
